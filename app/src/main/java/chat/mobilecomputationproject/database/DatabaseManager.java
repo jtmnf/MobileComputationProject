684d402ccea880cef952e54a738517145cc92f15
jtmnf <jtmnf@hotmail.com>
2015-12-20 22:08:28 +0000

    - DatabaseManager;

---
+++ app/src/main/java/chat/mobilecomputationproject/database/DatabaseManager.java
@@ -0,0 +1,42 @@
+package chat.mobilecomputationproject.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseManager extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "worldinchat.db";
+
+    private static final String LOGIN_TABLE = "login";
+    private static final String LOGIN_USER = "username";
+    private static final String LOGIN_PASS = "password";
+
+    private static final String CHAT_TABLE = "chat";
+    private static final String CHAT_MESSAGE = "message";
+    private static final String CHAT_USER = "username";
+
+    public DatabaseManager(Context context){
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        createDatabase(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private void createDatabase(SQLiteDatabase db){
+        try{
+            db = SQLiteDatabase.openDatabase(DATABASE_NAME, null, SQLiteDatabase.OPEN_READONLY);
+        } catch (Exception e){
+            //TODO - create tables here
+            // db.execSQL("create table " + TABLE_NAME + "(" + TITLE + " text, " + VALUE + " text)");
+
+        }
+    }
+}
